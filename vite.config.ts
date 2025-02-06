35de4a08449e44b8c89ea3cd2858f270458fa7ad
YOSSEF <YOSSEF@YOSSEF>
2025-02-06 12:56:35 +0300

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/1ST-Community/", // ضع اسم المستودع هنا
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
