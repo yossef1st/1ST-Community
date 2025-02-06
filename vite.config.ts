f511e05ad2ec687af7ed29cad3557c828cb4b539
YOSSEF <YOSSEF@YOSSEF>
2025-02-06 13:17:40 +0300

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/1ST-Community/', // أضف هذا السطر (يجب أن يكون مطابقًا لاسم المستودع)
+  base: '/1ST-Community/', // استخدم اسم المستودع هنا
 });
